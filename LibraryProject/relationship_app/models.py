592343a875b04540a380c568695e89b4be442523
Harold <haroldkaluba8@gmail.com>
2025-07-21 00:16:44 +0300

    tough add

---
+++ LibraryProject/relationship_app/models.py
@@ -0,0 +1,30 @@
+# relationship_app/models.py
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # A book has one author, an author can have many books
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book) # A library has many books, a book can be in many libraries
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE) # A librarian manages one library, a library has one librarian
+
+    def __str__(self):
+        return self.name
