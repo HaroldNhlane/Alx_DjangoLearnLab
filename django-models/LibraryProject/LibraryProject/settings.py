c63e8a0f9e33c057a559b08e8c9d170e9acf4f93
Harold <haroldkaluba8@gmail.com>
2025-07-22 10:28:55 +0300

    Migrations for 'relationship_app'

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf', # Add your new app here
+    'relationship_app', # Add your new app here
 ]
 
 
