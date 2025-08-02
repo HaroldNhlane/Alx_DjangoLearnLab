8d061767db3721fad1edb89eebe5d619ef0c83ff
Harold <haroldkaluba8@gmail.com>
2025-08-02 13:03:47 +0300

    Updated rest_framework.authtoken

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,15 @@
 # api_project/api/views.py
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# This is the view for your original 'books/' endpoint
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# This is the new ViewSet for all CRUD operations at 'books_all/'
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
