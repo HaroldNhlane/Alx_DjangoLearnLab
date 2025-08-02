4b7a81327327d492196044d577fa9aaa83dbfb51
Harold <haroldkaluba8@gmail.com>
2025-08-02 12:50:58 +0300

    Updated rest_framework.authtoken

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Third-party apps
-    #'rest_framework',
+    'rest_framework',
 
     # Your apps
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
