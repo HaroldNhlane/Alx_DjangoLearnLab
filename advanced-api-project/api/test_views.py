aa1402f84b5f9c9974a10c310765efc3c6a343f7
Harold <haroldkaluba8@gmail.com>
2025-08-08 02:30:58 +0300

    Modified api/test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,21 +3,56 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from api.models import Book # Don't forget this import!
+from django.contrib.auth.models import User
+from api.models import Book
 
 class BookListViewTest(APITestCase):
     def test_list_books_no_data(self):
         """
         Ensure the list view returns an empty list when no books exist.
         """
-        # Get the URL for the BookListView
         url = reverse('book-list')
-        
-        # Simulate a GET request to that URL
         response = self.client.get(url)
         
-        # Check that the status code is 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-        # Check that the response data is an empty list
         self.assertEqual(response.data, [])
+
+class BookCreateViewTest(APITestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        
+        # Get the URL for the BookCreateView
+        self.url = reverse('book-create')
+        
+        # Sample data for a new book
+        self.valid_data = {'title': 'The Lord of the Rings', 'author': 'J.R.R. Tolkien'}
+    
+    def test_create_book_authenticated_user(self):
+        """
+        Ensure an authenticated user can create a new book.
+        """
+        # Log in the user
+        self.client.login(username='testuser', password='password123')
+        
+        # Make a POST request with the test data
+        response = self.client.post(self.url, self.valid_data, format='json')
+        
+        # Assert that the request was successful
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # Assert that the book was added to the database
+        self.assertEqual(Book.objects.count(), 1)
+        
+    def test_create_book_unauthenticated_user(self):
+        """
+        Ensure an unauthenticated user cannot create a book.
+        """
+        # Do not log in the user
+        response = self.client.post(self.url, self.valid_data, format='json')
+        
+        # Assert that the request was forbidden
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+        # Assert that no book was added to the database
+        self.assertEqual(Book.objects.count(), 0)
