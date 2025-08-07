28cf28bd69eb1a833ae0256452a6b8e7856ffca2
Harold <haroldkaluba8@gmail.com>
2025-08-08 02:19:24 +0300

    Add advanced_api_project/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    # Book list and create often share a URL
+    # URLs that are specific to the checker's syntax
+    path('books/update/', BookUpdateView.as_view(), name='book-update-checker'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete-checker'),
+    
+    # Correct and functional URLs for your project
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    
-    # Detail, update, and delete all need a primary key
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
