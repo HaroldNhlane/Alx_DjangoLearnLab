4ee087ebff8454050e6c2114d857a39ac7d19dfe
Harold <haroldkaluba8@gmail.com>
2025-08-08 01:43:54 +0300

    Add protect endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly  # Combined import
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import generics
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
