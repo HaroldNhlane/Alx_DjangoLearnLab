b63f1434e28a7a5c9c8c2bf13ca3a51d378466cb
Harold <haroldkaluba8@gmail.com>
2025-08-16 12:15:47 +0300

    Add postgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,13 +74,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# File: django_blog/settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',      # Replace with your PostgreSQL database name
+        'USER': 'postgres',          # Replace with your PostgreSQL username
+        'PASSWORD': 'xolani',      # Replace with your PostgreSQL password
+        'HOST': 'localhost',               # Or the IP address of your PostgreSQL server
+        'PORT': '5432',                    # PostgreSQL's default port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
