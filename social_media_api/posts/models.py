c8f8d0a119a00322d2fc3f6a453301920df6bbfa
Harold <haroldkaluba8@gmail.com>
2025-08-20 11:33:08 +0300

    Add posts

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.conf import settings # Import settings to reference the user model
+
+class Post(models.Model):
+    """
+    A model representing a user's post on the social media platform.
+    """
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    """
+    A model representing a comment on a post.
+    """
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Your apps
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
