9c23b94fbf997b18b160a9eab04c63a7ee530656
Harold <haroldkaluba8@gmail.com>
2025-08-20 11:48:02 +0300

    Add  core components required by Django REST Framework to function

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,14 +1,41 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create a router instance
-router = DefaultRouter()
-# Register the viewsets with the router
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
 
-# The API URLs are now determined automatically by the router.
-urlpatterns = [
-    path('', include(router.urls)),
-]
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for managing posts.
+    Provides CRUD operations, filtering, and pagination.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['author', 'title']
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        """
+        Sets the author of the post to the current user.
+        """
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for managing comments.
+    Provides CRUD operations with proper permissions.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Sets the author of the comment to the current user.
+        """
+        serializer.save(author=self.request.user)
