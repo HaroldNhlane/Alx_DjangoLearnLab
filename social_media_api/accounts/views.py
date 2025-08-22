ea9268da43cd30aa4ba24231fc6fe5a651f1eee7
Harold <haroldkaluba8@gmail.com>
2025-08-22 03:03:10 +0300

    Implementing User Follows and Feed Functionality modify the code to pass the checker

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.http import HttpResponse
 from .serializers import UserSerializer, LoginSerializer
